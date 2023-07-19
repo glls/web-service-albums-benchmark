49ad2a89fe2821becf9f58890a9d38df6c60dccb
George Litos <gl@cyberpunk.gr>
2023-07-19 22:14:49 +0300

    Load albums once at start

--- rust-rocket/src/main.rs
+++ rust-rocket/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{get, routes};
+use rocket::{get, routes, State};
 use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
@@ -15,29 +15,21 @@ struct Album {
     Price: f64,
 }
 
+// Rocket state to hold the albums data
+struct AppState {
+    albums: Vec<Album>,
+}
+
 // Rocket handler to get all albums
 #[get("/albums")]
-fn get_albums() -> Json<Vec<Album>> {
-    let mut file = File::open("../albums.json").expect("Failed to open albums.json");
-    let mut content = String::new();
-    file.read_to_string(&mut content)
-        .expect("Failed to read albums.json");
-
-    let albums: Vec<Album> = serde_json::from_str(&content).expect("Failed to parse albums.json");
-    Json(albums)
+fn get_albums(state: State<AppState>) -> Json<Vec<Album>> {
+    Json(state.albums.clone())
 }
 
 // Rocket handler to get an album by ID
 #[get("/albums/<id>")]
-fn get_album_by_id(id: String) -> Option<Json<Album>> {
-    let mut file = File::open("albums.json").expect("Failed to open albums.json");
-    let mut content = String::new();
-    file.read_to_string(&mut content)
-        .expect("Failed to read albums.json");
-
-    let albums: Vec<Album> = serde_json::from_str(&content).expect("Failed to parse albums.json");
-
-    if let Some(album) = albums.iter().find(|album| album.ID == id) {
+fn get_album_by_id(id: String, state: State<AppState>) -> Option<Json<Album>> {
+    if let Some(album) = state.albums.iter().find(|album| album.ID == id) {
         Some(Json(album.clone()))
     } else {
         None
@@ -45,7 +37,15 @@ fn get_album_by_id(id: String) -> Option<Json<Album>> {
 }
 
 fn main() {
+    // Read the albums data from the file at the beginning
+    let mut file = File::open("../albums.json").expect("Failed to open albums.json");
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("Failed to read albums.json");
+    let albums: Vec<Album> = serde_json::from_str(&content).expect("Failed to parse albums.json");
+
+    // Create the Rocket application and store the albums data in the state
     rocket::ignite()
+        .manage(AppState { albums })
         .mount("/", routes![get_albums, get_album_by_id])
         .launch();
 }
