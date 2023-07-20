7d0175ec112c06d3833d647e47a6d6ebb65eb0be
George Litos <gl@cyberpunk.gr>
2023-07-20 11:36:04 +0300

    C# version with dotnet

---
+++ dotnet/MusicAlbumsApp/Controllers/AlbumsController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace YourAppName.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly List<Album> _albums;
+
+        public AlbumsController()
+        {
+            // Read albums data from albums.json file
+            var albumsData = System.IO.File.ReadAllText("../../albums.json");
+            _albums = JsonConvert.DeserializeObject<List<Album>>(albumsData);
+        }
+
+        [HttpGet]
+        public IEnumerable<Album> GetAlbums([FromQuery] int skip = 0, [FromQuery] int limit = 100)
+        {
+            return _albums.Skip(skip).Take(limit);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Album> GetAlbumById(string id)
+        {
+            var album = _albums.FirstOrDefault(a => a.ID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            return album;
+        }
+    }
+}
---
+++ dotnet/MusicAlbumsApp/Models/Album.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Album
+{
+    public string ID { get; set; }
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public double Price { get; set; }
+}
